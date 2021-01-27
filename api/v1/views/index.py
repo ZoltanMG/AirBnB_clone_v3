b14b41cf4c4b4ba23bb660727426a03a4e707490
daniela2001-png <dmbmora@outlook.com>
2021-01-27 12:34:50 -0500

    [Fixed] add commit to function

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,9 @@ from api.v1.views import app_views
 
 @app_views.route("/status", methods=["GET"])
 def index():
+    """
+    return the status of the api
+    """
     return {
         "status": "OK"
     }
