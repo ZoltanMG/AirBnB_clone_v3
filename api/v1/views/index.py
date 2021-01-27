a67a02de25088f184f1972b6b2d7de63334a905f
daniela2001-png <dmbmora@outlook.com>
2021-01-27 12:37:34 -0500

    [Fixed] add commit to function

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
+"""
+creamos nuestra primera vista con el blueprint
+que ya creamos
+"""
 
 
 @app_views.route("/status", methods=["GET"])
 def index():
     """
-    return the status of the api
+    esta vista retorna el estado de la api
     """
     return {
         "status": "OK"
