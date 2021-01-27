c40340c53008e042b06f0012a8b4d9b9e57c6d2f
daniela2001-png <dmbmora@outlook.com>
2021-01-27 18:00:17 +0000

    [Update] changes the return of the route function /status using jsonify from flask

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
+from flask import jsonify
+
 """
 creamos nuestra primera vista con el blueprint
 que ya creamos
@@ -12,6 +14,4 @@ def index():
     """
     esta vista retorna el estado de la api
     """
-    return {
-        "status": "OK"
-    }
+    return jsonify({'status': 'OK'})
